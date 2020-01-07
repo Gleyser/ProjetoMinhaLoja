4d1143e1157fa4fbdc86e3b452cb9d909710ee36
Gleyser Guimarães <gleyser.bonfim.guimaraes@ccc.ufcg.edu.br>
2020-01-07 09:28:27 -0300

    inserindo caelum.stella

--- lojaweb/src/main/java/br/com/gleyser/maven/lojaweb/ContatoServlet.java
+++ lojaweb/src/main/java/br/com/gleyser/maven/lojaweb/ContatoServlet.java
@@ -9,13 +9,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.caelum.stella.tinytype.CPF;
+
 @WebServlet(urlPatterns={"/contato"})
 public class ContatoServlet extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		PrintWriter writer = resp.getWriter();
-        writer.println("<html><h2>Entre em contato</h2></html>");
+		new CPF("2222222222").isValido();
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html><h2>Bata um papo conosco</h2></html>");
         writer.close();
 	}
 
